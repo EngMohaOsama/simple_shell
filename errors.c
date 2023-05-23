0ac9d0162733f9f9ec2c855e27c02b5f04b650da
snwakosi <snwakosi@gmail.com>
2023-05-23 06:33:39 -0700

    simple shell project

---
+++ errors.c
@@ -0,0 +1,87 @@
+#include "shell.h"
+
+/**
+ *_eputs - to print a type-in string
+ * @str: parameter to be printed
+ *
+ * Return: Nill
+ */
+
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar - writes the c parameter to stderr
+ * @c: parameter to print
+ *
+ * Return: if successful return 1.
+ * On error, return -1 and set errno appropriately.
+ */
+
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - writes the character c to given fd
+ * @c: The character to print
+ * @fd: The filedescriptor to write to
+ *
+ * Return: if successful, return 1.
+ * On error, return -1 and set appropriately.
+ */
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ *_putsfd - prints an input string of characters
+ * @str: the string parameters to be printed
+ * @fd: file signifier we write to
+ *
+ * Return: number of charput
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
---
+++ exits.c
@@ -0,0 +1,77 @@
+#include "shell.h"
+
+/**
+ **_strncpy - duplicates a string
+ *@dest: the destination of the string parameters to be copied to
+ *@src: the strings source
+ *@n: number of characters to copy
+ *Return: the joined  string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - combine two strings
+ *@dest: initial string
+ *@src: second string
+ *@n: quantity of bytes to be used up
+ *Return: the joined string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - finds a character in a string
+ *@s: string parameter to be parsed
+ *@c: character to find
+ *Return: a pointer to the memory area s
+ */
+
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
